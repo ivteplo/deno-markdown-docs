0f82d96c075f79259c96f227cec875f1cb6943df
Ivan Teplov <iteplov7@gmail.com>
2024-06-28 17:10:43 +0200

    Parse command line arguments and add file output support

--- app/generateDocumentation.js
+++ app/generateDocumentation.js
@@ -6,26 +6,62 @@
 
 import { doc as readJSDoc } from "deno_doc"
 import * as path from "deno:path"
+import yargs from "yargs"
 
 import { DocumentationGenerator } from "../mod.js"
 
-async function main() {
-	const inputFile = Deno.args?.[0]
+function parseArgs() {
+	const args = yargs(Deno.args)
+		.command("*", "generate documentation for a JavaScript file")
+		.positional("input-file", {
+			describe: "file to document",
+			demandOption: true,
+			type: "string"
+		})
+		.option("output", {
+			alias: "o",
+			type: "string",
+			description: "Output file path",
+			default: ""
+		})
+		.parse()
 
-	if (!inputFile) {
+	return {
+		inputFiles: args._,
+		outputFile: args.output
+	}
+}
+
+async function main() {
+	const { inputFiles, outputFile } = parseArgs()
+
+	if (!inputFiles) {
 		console.error("No input file specified")
 		Deno.exit(1)
 	}
 
-	const inputFileURLString = path.toFileUrl(path.resolve(inputFile)).toString()
-	const documentationInJSON = await readJSDoc(inputFileURLString)
+	const documentationInJSON = (await Promise.all(
+		inputFiles
+			.map(inputFile => path.toFileUrl(path.resolve(inputFile)).toString())
+			.map(inputFileURLString => readJSDoc(inputFileURLString))
+	)).flat()
 
 	const documentationInMarkdown = documentationInJSON
 		.filter(DocumentationGenerator.isObjectDocumentationToBeDisplayed)
 		.map(object => DocumentationGenerator.generate(object))
 		.join("\n\n\n\n") + "\n"
 
-	console.log(documentationInMarkdown)
+	if (outputFile === "") {
+		console.log(documentationInMarkdown)
+	} else {
+		try {
+			await Deno.writeTextFile(outputFile, documentationInMarkdown)
+		} catch (error) {
+			console.error("Could not save documentation to the file:", error.toString())
+			console.error("Printing the output to the console")
+			console.log(documentationInMarkdown)
+		}
+	}
 }
 
-await main()
+main().catch(console.error)
